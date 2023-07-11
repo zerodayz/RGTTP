cdd7a28cdf2d793c12e2080cce87d819182c53b4
Robin Cernin <rcernin@redhat.com>
2023-07-11 15:45:05 +1000

    update ex2

--- ex2.py
+++ ex2.py
@@ -1,8 +1,4 @@
-# 1. Make a shopping list of a 5 items and print it out.
-
-### INSERT YOUR CODE HERE
-
-# 2. Here is my shopping list:
+# Here is my shopping list:
 #
 # 2.99 Apples
 # 1.79 Milk
@@ -10,7 +6,11 @@
 # 6.99 Chicken
 # 2.49 Pasta
 
-# Use python as your calculator and print out the total cost.
+# 1. Make a python list of the 5 items above and print it out.
+
+### INSERT YOUR CODE HERE
+
+# 2. Use python as your calculator and print out the total cost of all items on shopping list.
 
 ### INSERT YOUR CODE HERE
 
