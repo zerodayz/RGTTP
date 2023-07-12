30f6f69e16bc833aefd7c1d8b74016b4fde5bb30
Robin Cernin <rcernin@redhat.com>
2023-07-12 13:30:48 +1000

    update

--- ex2.py
+++ ex2.py
@@ -12,18 +12,18 @@
 
 # 1. Make a python list of the 5 items above and print it out.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 2. Use python as your calculator and print out the total cost of
 #    all items on shopping list.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 3. I have decided that we need 5 cartons of milk, can you recalculate
 #    it and print it out again?
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 4. Print out every item of your shopping list on a new line.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
--- ex4.py
+++ ex4.py
@@ -5,34 +5,34 @@
 # 1. Create a new list with the following values:
 #    Apple, Milk, Bread, Chicken and Pasta
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 2. Write a check to see if Orange is in the shopping list or not.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 3. Write a function "in_shopping_list" that takes a item such as
 #    orange, and returns True or False.
 #    Depending whether the item is in the list or not.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 4. Write a function "show_shopping_list" that will return a
 #    shopping list and print it out on the screen.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 5. Create a list of the following values: 2.99 1.79 3.49 6.99 2.49
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 6. Create a function price_checker, and pass this list as
 #    an argument and let the function return the cheapest price.#
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
 
 # 7. Write another function show_price, that will call your
 #    price_checker function and uses the result to
 #    print the cheapest price.
 
-### INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
